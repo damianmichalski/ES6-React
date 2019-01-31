02f7732536d258df4f8b162f6fa091daff1a5db2
DamianM <damian.michalski@kramp.com>
2019-01-31 13:41:33 +0100

    ES6- timer on React

---
+++ script.js
@@ -0,0 +1,111 @@
+class Stopwatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      times: {
+        minutes: 0,
+        seconds: 0,
+        miliseconds: 0
+      },
+      results: []
+    }
+    this.running = false;
+  }
+  reset() {
+    this.setState({
+      times: {
+        minutes: 0,
+        seconds: 0,
+        miliseconds: 0
+      }
+    })
+  }
+
+  format(times) {
+    return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+  }
+
+  start() {
+    if (!this.state.running) {
+      this.running = true;
+      this.watch = setInterval(() => this.step(), 10);
+    }
+  }
+
+  step() {
+    if (!this.running) return;
+    this.calculate();
+  }
+
+  calculate() {
+    let { minutes, seconds, miliseconds } = this.state.times;
+    miliseconds += 1;
+    if (miliseconds >= 100) {
+      seconds += 1;
+      miliseconds = 0;
+    }
+    if (seconds >= 60) {
+      minutes += 1;
+      seconds = 0;
+    }
+    this.setState({
+      times: {
+        minutes,
+        seconds,
+        miliseconds
+      }
+    })
+  }
+
+  stop() {
+    this.running = false;
+    clearInterval(this.watch);
+  }
+
+  resetwatch() {
+    this.running = false;
+    this.reset();
+    clearInterval(this.watch);
+  }
+
+  save() {
+    const results = this.state.results.slice();
+    results.push(this.format(this.state.times));
+    this.setState({results: results});
+  }
+
+  clear() {
+    this.setState({results: [] });
+  }
+
+  render() {
+    return (
+        <div className={'app'}>
+          <nav className={'controls'}>
+            <a href="#" onClick={this.start.bind(this)}>Start</a>
+            <a href="#" onClick={this.stop.bind(this)}>Stop</a>
+            <a href="#" onClick={this.reset.bind(this)}>Reset</a>
+            <a href="#" onClick={this.save.bind(this)}>Save</a>
+            <a href="#" onClick={this.clear.bind(this)}>Clear</a>
+          </nav>
+          <div className={'stopwatch'}>
+            {this.format(this.state.times)}</div>
+          <ul className={'results'}>
+            {this.state.results.map(result => <li>{result}</li>)}</ul>
+        </div>
+    )
+  }
+}
+
+function pad0(value) {
+  let result = value.toString();
+  if (result.length < 2) {
+    result = '0' + result;
+  }
+  return result;
+}
+
+ReactDOM.render(
+    <Stopwatch/>,
+    document.getElementById('app')
+);
